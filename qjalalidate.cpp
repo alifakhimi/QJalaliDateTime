fa0ed74445ab106a210f38b4342dd90ca105cc9a
Fakhimi <fakhimi@gmail.com>
2017-02-14 10:04:24 +0330

    first commit

--- qjalalidate.cpp
+++ qjalalidate.cpp
@@ -1,6 +1,246 @@
 #include "qjalalidate.h"
 
-QJalaliDate::QJalaliDate(QObject *parent) : QObject(parent)
+QJalaliDate::QJalaliDate(int year, int month, int day) : jd(nullJd())
 {
+    setDate(year, month, day);
+}
 
+QJalaliDate QJalaliDate::currentDate()
+{
+    QDate gd = QDate::currentDate();
+    QJalaliDate d;
+    qint64 j = jalaliDayFromDate(gd.year(), gd.month(), gd.day());
+    d.jd = j;
+    return d;
+}
+
+QJalaliDate QJalaliDate::fromGregorianDay(qint64 jd)
+{
+}
+
+QDate QJalaliDate::toGregorian(const QJalaliDate &jd)
+{
+}
+
+QJalaliDate QJalaliDate::fromGregorian(const QDate &gdate)
+{
+    return fromGregorian(gdate.year(), gdate.month(), gdate.day());
+}
+
+QJalaliDate QJalaliDate::fromGregorian(int year, int month, int day)
+{
+    return QJalaliDate(year, month, day);
+}
+
+QJalaliDate QJalaliDate::fromString(const QString &string, Qt::DateFormat format)
+{
+}
+
+QJalaliDate QJalaliDate::fromString(const QString &string, const QString &format)
+{
+}
+
+bool QJalaliDate::isLeapYear(int year)
+{
+    int mod;
+    mod = year % 33;
+    if (year <= 1472 && year >= 1343) {
+        if (mod == 1 || mod == 5 || mod == 9 || mod == 13 || mod == 17 || mod == 22 || mod == 26 || mod == 23)
+            return true;
+        else
+            return false;
+    }
+
+    if (year <= 1342 && year >= 1244) {
+        if (mod == 1 || mod == 5 || mod ==9 || mod == 13 || mod == 17 || mod == 22 || mod == 26 || mod == 23)
+            return true;
+        else
+            return false;
+    }
+}
+
+bool QJalaliDate::isValid(int year, int month, int day)
+{
+    return true;
+}
+
+int QJalaliDate::day() const
+{
+    if (isNull())
+        return 0;
+
+    return getDateFromJalaliDay(jd).day;
+}
+
+/*!
+    Returns the number corresponding to the month of this date, using
+    the following convention:
+    \list
+    \li 1 = "January"
+    \li 2 = "February"
+    \li 3 = "March"
+    \li 4 = "April"
+    \li 5 = "May"
+    \li 6 = "June"
+    \li 7 = "July"
+    \li 8 = "August"
+    \li 9 = "September"
+    \li 10 = "October"
+    \li 11 = "November"
+    \li 12 = "December"
+    \endlist
+    Returns 0 if the date is invalid.
+    \sa year(), day()
+*/
+int QJalaliDate::month() const
+{
+    if (isNull())
+        return 0;
+
+    return getDateFromJalaliDay(jd).month;
+}
+
+int QJalaliDate::year() const
+{
+    if (isNull())
+        return 0;
+
+    return getDateFromJalaliDay(jd).year;
+}
+
+ParsedDate QJalaliDate::getDateFromJalaliDay(qint64 jalaliDay)
+{
+    int jNp = div(jalaliDay, 12053).quot; // 12053 = 365 * 33 + 32 / 4
+
+    jalaliDay = jalaliDay % 12053;
+
+    int year = 979 + 33 * jNp + 4 * div(jalaliDay, 1461).quot; // 1461 = 365 * 4 + 4 / 4
+
+    jalaliDay %= 1461;
+
+    if(jalaliDay >= 366) {
+        year += div(jalaliDay - 1, 365).quot;
+        jalaliDay = (jalaliDay - 1) % 365;
+    }
+
+    int ii;
+    for(int i = 0; i < 11 && jalaliDay >= jalaliDaysInMonth[i]; ++i){
+        jalaliDay -= jalaliDaysInMonth[i];
+        ii = i;
+    }
+
+    int  month = ii + 2;
+    int  day = jalaliDay + 1;
+
+    const ParsedDate result = { year, month, day };
+    return result;
+}
+
+int QJalaliDate::dayOfWeek() const
+{
+}
+
+int QJalaliDate::dayOfYear() const
+{
+}
+
+int QJalaliDate::daysInMonth() const
+{
+}
+
+int QJalaliDate::daysInYear() const
+{
+}
+
+qint64 QJalaliDate::daysTo(const QJalaliDate &d) const
+{
+}
+
+qint64 QJalaliDate::daysTo(const QDate &d) const
+{
+}
+
+void QJalaliDate::getDate(int *year, int *month, int *day) const
+{
+}
+
+bool QJalaliDate::setDate(int year, int month, int day)
+{
+    if (isValid(year, month, day))
+        jd = jalaliDayFromDate(year, month, day);
+    else
+        jd = nullJd();
+
+    return isValid();
+}
+
+QString QJalaliDate::toString(const QString &format) const
+{
+    const ParsedDate d = getDateFromJalaliDay(jd);
+
+    return QString(format).arg(d.year, 4, 10, QChar('0')).arg(d.month, 2, 10, QChar('0')).arg(d.day, 2, 10, QChar('0'));
+
+//    QStringList jalali;
+//    jalali << QString::number(jy) << QString::number(jm) << QString::number(jd); // << m_jalaliMonthsName[jm] ;
+//    return jalali.join('-');
+}
+
+QString QJalaliDate::toString(Qt::DateFormat format) const
+{
+//    if (!isValid())
+//        return QString();
+
+//    switch (format) {
+//    case Qt::SystemLocaleDate:
+//    case Qt::SystemLocaleShortDate:
+//        return QLocale::system().toString(*this, QLocale::ShortFormat);
+//    case Qt::SystemLocaleLongDate:
+//        return QLocale::system().toString(*this, QLocale::LongFormat);
+//    case Qt::LocaleDate:
+//    case Qt::DefaultLocaleShortDate:
+//        return QLocale().toString(*this, QLocale::ShortFormat);
+//    case Qt::DefaultLocaleLongDate:
+//        return QLocale().toString(*this, QLocale::LongFormat);
+//    case Qt::RFC2822Date:
+//        return QLocale::c().toString(*this, QStringLiteral("dd MMM yyyy"));
+//    default:
+//#ifndef QT_NO_TEXTDATE
+//    case Qt::TextDate:
+//        return toStringTextDate(*this);
+//#endif
+//    case Qt::ISODate:
+//    case Qt::ISODateWithMs:
+//        return toStringIsoDate(jd);
+//    }
+}
+
+int QJalaliDate::weekNumber(int *yearNumber) const
+{
+}
+
+QString QJalaliDate::getName() const
+{
+    return m_name;
+}
+
+qint64 QJalaliDate::jalaliDayFromDate(int year, int month, int day)
+{
+    year = year - 1600;
+    month = month - 1;
+    day = day - 1;
+
+    int gDayNo = 365 * year + div(year + 3, 4).quot - div(year + 99, 100).quot + div(year + 399, 400).quot;
+
+    for(int i=0; i < month; ++i)
+        gDayNo += gregorianDaysInMonth[i];
+
+    if(month > 1 && ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)))
+        //leap and after Feb
+        gDayNo++;
+
+    gDayNo += day;
+
+    qint64 jDayNo = gDayNo - 79;
+
+    return jDayNo;
 }
--- qjalalidate.h
+++ qjalalidate.h
@@ -1,17 +1,107 @@
 #ifndef QJALALIDATE_H
 #define QJALALIDATE_H
 
-#include <QObject>
+#include <QDate>
+#include <QStringList>
+#include <QList>
+#include <QDebug>
 
-class QJalaliDate : public QObject
+struct ParsedDate
 {
-    Q_OBJECT
+    int year, month, day;
+};
+
+class QJalaliDate
+{
 public:
-    explicit QJalaliDate(QObject *parent = 0);
+    enum MonthNameType {
+        DateFormat = 0,
+        StandaloneFormat
+    };
 
-signals:
+private:
+    explicit QJalaliDate(qint64 jalaliDay) : jd(jalaliDay) {}
 
-public slots:
+public:
+    QJalaliDate() : jd(nullJd()) { }
+    //QJalaliDate(const QDate &date) : jd(nullJd()) { }
+    QJalaliDate(int year, int month, int day);
+
+    static QJalaliDate currentDate();
+    static QJalaliDate fromGregorianDay(qint64 jd);
+    static QJalaliDate fromGregorian(const QDate &gdate);
+    static QJalaliDate fromGregorian(int year, int month, int day);
+    static QDate toGregorian(const QJalaliDate &jd);
+    static QJalaliDate fromString(const QString &string, Qt::DateFormat format = Qt::TextDate);
+    static QJalaliDate fromString(const QString &string, const QString &format);
+    static bool isLeapYear(int year);
+    static bool isValid(int year, int month, int day);
+
+    int day() const;
+    int	month() const;
+    int	year() const;
+
+    static ParsedDate getDateFromJalaliDay(qint64 jalaliDay);
+
+    int dayOfWeek() const;
+    int dayOfYear() const;
+    int daysInMonth() const;
+    int daysInYear() const;
+    qint64 daysTo(const QJalaliDate &d) const;
+    qint64 daysTo(const QDate &d) const;
+    void getDate(int *year, int *month, int *day) const;
+    bool setDate(int year, int month, int day);
+    QString	toString(const QString &format) const;
+    QString	toString(Qt::DateFormat format = Qt::TextDate) const;
+    int	weekNumber(int *yearNumber = Q_NULLPTR) const;
+
+    bool isNull() const { return !isValid(); }
+    bool isValid() const { return jd >= minJd() && jd <= maxJd(); }
+    QString getName() const;
+
+    QJalaliDate addDays(qint64 ndays) const;
+    QJalaliDate addMonths(int nmonths) const;
+    QJalaliDate addYears(int nyears) const;
+
+    bool operator==(const QJalaliDate &other) const { return jd == other.jd; }
+    bool operator!=(const QJalaliDate &other) const { return jd != other.jd; }
+    bool operator< (const QJalaliDate &other) const { return jd <  other.jd; }
+    bool operator<=(const QJalaliDate &other) const { return jd <= other.jd; }
+    bool operator> (const QJalaliDate &other) const { return jd >  other.jd; }
+    bool operator>=(const QJalaliDate &other) const { return jd >= other.jd; }
+
+private:
+    // using extra parentheses around min to avoid expanding it if it is a macro
+    static inline qint64 nullJd() { return (std::numeric_limits<qint64>::min)(); }
+    static inline qint64 minJd() { return Q_INT64_C(-784350574879); }
+    static inline qint64 maxJd() { return Q_INT64_C( 784354017364); }
+
+    static inline qint64 jalaliDayFromDate(int year, int month, int day);
+
+    qint64 jd; // Jalali Date
+    QString m_name = "جلالی";
+};
+
+static const char gregorianDaysInMonth[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+static const char jalaliDaysInMonth[] =    { 31, 31, 31, 31, 31, 31, 30, 30, 30, 30, 30, 29 };
+
+static const QString jalaliMonthsName[] =
+{
+    QString::fromUtf8("فروردین"), QString::fromUtf8("اردیبهشت"), QString::fromUtf8("خرداد"),
+    QString::fromUtf8("تیر"), QString::fromUtf8("مرداد"), QString::fromUtf8("شهریور"),
+    QString::fromUtf8("مهر"), QString::fromUtf8("آبان"), QString::fromUtf8("آذر"),
+    QString::fromUtf8("دی"), QString::fromUtf8("بهمن"), QString::fromUtf8("اسفند")
+};
+
+static const QString jalaliDaysName[] =
+{
+    QString::fromUtf8("شنبه"),
+    QString::fromUtf8("یکشنبه"),
+    QString::fromUtf8("دوشنبه"),
+    QString::fromUtf8("سه شنبه"),
+    QString::fromUtf8("چهارشنبه"),
+    QString::fromUtf8("پنجشنبه"),
+    QString::fromUtf8("آدینه")
 };
 
 #endif // QJALALIDATE_H
