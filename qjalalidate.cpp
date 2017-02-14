68fba39206b61ffc05b2f71f0f9535f1a03d4fdd
Fakhimi <fakhimi@gmail.com>
2017-02-14 10:03:51 +0330

    Initial commit

---
+++ qjalalidate.cpp
@@ -0,0 +1,6 @@
+#include "qjalalidate.h"
+
+QJalaliDate::QJalaliDate(QObject *parent) : QObject(parent)
+{
+
+}
---
+++ qjalalidate.h
@@ -0,0 +1,17 @@
+#ifndef QJALALIDATE_H
+#define QJALALIDATE_H
+
+#include <QObject>
+
+class QJalaliDate : public QObject
+{
+    Q_OBJECT
+public:
+    explicit QJalaliDate(QObject *parent = 0);
+
+signals:
+
+public slots:
+};
+
+#endif // QJALALIDATE_H
